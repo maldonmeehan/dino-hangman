514d3892e53117ebf3d9866ca4cc98a29fbb5d14
Lauren Posey and Maldon Meehan <lauren@>
2016-08-03 13:37:12 -0700

    display your dino ipsum because yay

--- js/dino-interface.js
+++ js/dino-interface.js
@@ -2,7 +2,7 @@ var Dino = require('./../js/dino.js').dinoModule;
 
 $(document).ready(function() {
   $('.dino_ipsum_form').submit(function(event) {
-    event.preventDefault()
+    event.preventDefault();
     var currentDinoObject = new Dino();
     var dinoPara = parseInt($('#paragraph').val());
     var dinoWord = parseInt($('#word').val());
--- js/dino.js
+++ js/dino.js
@@ -3,15 +3,8 @@ function Dino(){
 
 Dino.prototype.getDino = function(dinoPara, dinoWord) {
   $.get('http://dinoipsum.herokuapp.com/api/?format=html&paragraphs=3&words=15',function(response) {
-    // fillContainer = function(html) {
-    //   $('#dino-container').html(html);
-    // };
-    console.log(response);
+    $("#dino-container").prepend(response);
   });
-  // .fail(function(error) {
-  //   $('.showDino').text(error.responseJSON.message);
-  //   console.log('Where did all the dinosaurs go?');
-  // });
 };
 
 exports.dinoModule = Dino;
